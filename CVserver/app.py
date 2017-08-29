dfea15fb0c92ebacdd947e51103b0afb4a87e860
VibAltekar <altekar.vibhav@gmail.com>
2017-08-28 23:23:30 -0700

    test1

---
+++ CVserver/app.py
@@ -0,0 +1,29 @@
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+app = ClarifaiApp()
+import os
+from flask import Flask
+from clarifai.rest import ClarifaiApp
+import base64
+# get the general model
+# export CLARIFAI_API_KEY=f0bc502594a44e3986dd48ee7530aa22
+app = Flask(__name__)
+
+@app.route("/caption/<string>")
+def image_caption(string):
+    decoded_image = base64.b64decode(string)
+    app = ClarifaiApp(api_key='{api-key}')
+    model = app.models.get("general-v1.3")
+    image = ClImage(file_obj=open(decoded_image, 'rb'))
+    model.predict([image])
+    print(os.popen("pwd").read())
+    os.system("ls")
+    return "lmao"
+
+@app.route("/")
+def main():
+    return "hello"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
